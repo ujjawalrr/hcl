df2fbe8dadbe87112d5baac519d17df778652388
Abhishek Singh <asingh19082000@gmail.com>
2025-03-01 09:59:46 +0530

    backend setup

---
+++ hcl-backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { connectMongoDB } = require("./connection");
+const dotenv = require("dotenv")
+dotenv.config()
+const app = express();
+const PORT = 3000;
+
+//mongodb connection
+connectMongoDB(process.env.MONGO).
+    then(() => {
+        console.log(`MongoDb Connected`);
+    }).
+    catch((err) => {
+        console.log("Mongo Error", err);
+    })
+
+//middleware
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+})
